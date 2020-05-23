5deace1dde5089379370a5d3b27370388b68adf2
Ricardo González Pacheco <rigonpa@gmail.com>
2020-05-23 20:38:36 +0200

    Fixing lastPostedAt variable

---
+++ DiscourseClient/Extensions/UIImage+Extension.swift
@@ -0,0 +1,37 @@
+//
+//  UIImage+Extension.swift
+//  AvengersApp
+//
+//  Created by Ricardo González Pacheco on 25/04/2020.
+//  Copyright © 2020 Ricardo González Pacheco. All rights reserved.
+//
+
+import UIKit
+
+extension Coordinator {
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else { fatalError() }
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+}
